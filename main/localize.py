cafe6ab225913a7b58d479c4f6cb10f3bfc6c247
anna-maria21 <strelchenko2010amg@gmail.com>
2024-10-11 18:21:13 +0300

    change localization logic

--- main/localize.py
+++ main/localize.py
@@ -1,35 +1,23 @@
 import cv2
 from rest_framework import status
 from rest_framework.response import Response
-from ultralytics import YOLO
 
+from django.apps import apps
 from main.classify import classify
+import main.utils as utils
 
 
-def localize(image, localization_model_str, classification_model_str):
-    yolo_model = '' #YOLO('D:/магістерська/pcb_defects_detection/main/ai-models/my-model.pt')
-    vgg16_model = ''
-    models_dict = {'1': yolo_model, '3': vgg16_model}
+def localize(image_path, localization_model_str, classification_model_str):
+    models_dict = apps.get_app_config('main').models_dict
     localization_model = models_dict[localization_model_str]
     classification_model = models_dict[classification_model_str]
 
-    # result = localization_model(image)
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # cropped_image = crop_image(image, result)
-
-
-def crop_image(image, results):
-    img = cv2.imread(image)
-
-    detections = results[0].boxes
-
-    if detections:
-        for box in detections:
-            x_min, y_min, x_max, y_max = map(int, box.xyxy[0])
-            cropped_img = img[y_min:y_max, x_min:x_max]
-
-            cropped_img_path = 'D:/магістерська/pcb_defects_detection/main/cropped_images/' + image.split('/')[-1]
-            cv2.imwrite(cropped_img_path, cropped_img)
-            classification_result = classify(cropped_img_path)
-
-    return classification_result
+    if localization_model == models_dict['1']:
+        boxes = utils.detect_with_yolo(localization_model, utils.preprocess_for_yolo(image_rgb))
+    else:
+        boxes = utils.detect_with_fasterrcnn(localization_model, utils.preprocess_for_fasterrcnn(image_rgb))
+    cropped_image = utils.crop_image(image_path, boxes)
+    return cropped_image
